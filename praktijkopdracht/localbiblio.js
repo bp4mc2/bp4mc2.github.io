89a14b277072cd55f02daa43fbf0fa5742796162
architolk <msj@xs4all.nl>
2025-03-07 08:26:19 +0100

    Added local biblio

---
+++ praktijkopdracht/localbiblio.js
@@ -0,0 +1,12 @@
+var localBiblio = {
+  "MvtSv": {
+    title: "Memorie van toelichting bij het nieuwe wetboek van strafvordering",
+  },
+  "Rechtspraak": {
+    title: "Juridische begrippen - Rechtspraak",
+    href: "https://www.rechtspraak.nl/juridische-begrippen/Paginas/default.aspx",
+  },
+  "Sv": {
+    title: "Nieuw Wetboek van Strafvordering",
+  },
+}
