edcbf6cc608e650b864efd0c3d523fbb3a1624f7
architolk <msj@xs4all.nl>
2025-03-13 15:31:35 +0100

    Update biblio

--- praktijkopdracht/localbiblio.js
+++ praktijkopdracht/localbiblio.js
@@ -1,12 +1,273 @@
 var localBiblio = {
-  "MvtSv": {
-    title: "Memorie van toelichting bij het nieuwe wetboek van strafvordering",
+  "AP": {
+    title: "Autoriteit Persoonsgegevens",
+    href: "https://www.autoriteitpersoonsgegevens.nl/themas/basis-avg/privacy-en-persoonsgegevens/wat-zijn-persoonsgegevens#:~:text=Genetische%20gegevens.,maakt%20de%20informatie%20zo%20gevoelig.",
+  },
+  "Aanwijzinginbeslagneming": {
+    title: "Aanwijzing inbeslagneming",
+    href: "https://wetten.overheid.nl/BWBR0027785/2010-07-01",
+  },
+  "Aanwijzingtbs": {
+    title: "Aanwijzing tbs met voorwaarden en voorwaardelijke beëindiging van het bevel tot verpleging van overheidswege",
+    href: "https://wetten.overheid.nl/BWBR0044687/2021-03-01",
+  },
+  "Advw": {
+    title: "Advocatenwet",
+  },
+  "Adw": {
+    title: "Algemene Douanewet",
+  },
+  "AmbtsinstructiePolitie": {
+    title: "Ambtsinstructie voor de politie, de Koninklijke marechaussee en andere opsporingsambtenaren",
+  },
+  "Artikel11lid12EU": {
+    title: "Handvest van de grondrechten van de Europese Unie",
+    href: "https://fra.europa.eu/nl/eu-charter/article/11-de-vrijheid-van-meningsuiting-en-van-informatie",
+  },
+  "AwB": {
+    title: "Algemene wet bestuursrecht",
+    href: "https://wetten.overheid.nl/BWBR0005537/2025-02-04",
+  },
+  "Awb": {
+    title: "Algemene wet bestuursrecht",
+  },
+  "BR": {
+    title: "Snijders, GS Burgerlijke Rechtsvordering, titel Negende titel Rv, aant. 1",
+    href: "https://jwsii.ivdnt.org/docs/buitengewone%20rechtsmiddelen/_8048",
+  },
+  "BW": {
+    title: "Burgerlijk Wetboek",
+    href: "https://wetten.overheid.nl/BWBR0002656/2024-01-01",
+  },
+  "Bw": {
+    title: "Burgerlijk Wetboek",
+    href: "https://wetten.overheid.nl/BWBR0002656/2024-01-01",
+  },
+  "CBS": {
+    title: "Centraal Bureau voor de Statistiek",
+    href: "https://www.cbs.nl/nl-nl",
+  },
+  "Cbsnl": {
+    title: "Begrippen Centraal Bureau voor de Statistiek",
+  },
+  "DagelijksStrafrecht": {
+    title: "Dagelijks Strafrecht",
+    href: "https://dagelijksrecht.wixsite.com/recht",
+  },
+  "DeNationaleAdviesbalie": {
+    title: "De Nationale Adviesbalie",
+    href: "https://www.nab.nl/",
+  },
+  "ECLINLHR2001AB2202": {
+    title: "ECLI:NL:HR:2001:AB2202",
+  },
+  "ECLINLHR2011BP0291": {
+    title: "ECLI:NL:HR:2011:BP0291",
+    href: "https://uitspraken.rechtspraak.nl/details?id=ECLI:NL:HR:2011:BP0291",
+  },
+  "GW": {
+    title: "Grondwet",
+    href: "https://wetten.overheid.nl/BWBR0001840/2023-02-22",
+  },
+  "GemeenteAmsterdam": {
+    title: "Bijlage 1 bij protocol Alcohol-, drugs- en medicijngebruik: Wet- en regelgeving",
+  },
+  "Gw": {
+    title: "Grondwet",
+  },
+  "Handboekvoorstrafzaken": {
+    title: "Handboek voor strafzaken",
+  },
+  "JuridischWoordenboek": {
+    title: "Juridisch Woordenboek",
+    href: "https://amo.institute/index.php",
+  },
+  "KamerstukkenII20040526671nr7": {
+    title: "Kamerstukken II 2004/05, 26671, nr. 7, p. 35",
+  },
+  "KamerstukkenII201314339893": {
+    title: "Kamerstukken II 2013/14, 33989, 3, p. 8",
+  },
+  "LawInsider": {
+    title: "Intimate parts definition",
+    href: "https://www.lawinsider.com/dictionary/intimate-parts",
+  },
+  "MvT": {
+    title: "Memorie van toelichting nieuw Wetboek van Strafvordering",
+  },
+  "NFI-1": {
+    title: "Herkomstonderzoek op basis van isotopen",
+    href: "https://www.forensischinstituut.nl/forensisch-onderzoek/herkomstonderzoek-op-basis-van-isotopen",
+  },
+  "NFI-2": {
+    title: "Toxicologie (Forensische)",
+    href: "https://www.forensischinstituut.nl/forensisch-onderzoek/toxicologie-forensische#:~:text=Toxicologie%20is%20de%20wetenschap%20die,rijden%20onder%20invloed%20en%20bedwelming.",
+  },
+  "NFI-3": {
+    title: "Vakbijlage Vergelijkend handschriftonderzoek",
+    href: "https://www.forensischinstituut.nl/publicaties/publicaties/2017/10/18/vakbijlage-vergelijkend-handschriftonderzoek",
+  },
+  "NFI-4": {
+    title: "Vergelijkend Spraakonderzoek",
+    href: "https://www.forensischinstituut.nl/publicaties/publicaties/2020/02/03/vakbijlage-vergelijkend-spraakonderzoek",
+  },
+  "NFI-5": {
+    title: "Vergelijkend onderzoek",
+    href: "https://www.forensischinstituut.nl/over-het-nfi/organisatie/organisatiestructuur",
+  },
+  "NFPO": {
+    title: "Haaranalyse door het Nederlands Forensisch Instituut bij het vermoeden van een aan drug of geneesmiddel gerelateerd misdrijf",
+    href: "https://www.npfo.nl/assets/archive_item/pdf/8b903dcba5bd7f5594e490b6aa51bdbd_2011wp0187.pdf",
+  },
+  "NaturePortfolio": {
+    title: "Microbiology articles from across Nature Portfolio",
+    href: "https://www-nature-com.translate.goog/subjects/microbiology?error=cookies_not_supported&code=b35b1c7e-484c-486e-9488-fdf9450364c2&_x_tr_sl=en&_x_tr_tl=nl&_x_tr_hl=nl&_x_tr_pto=rq#:~:text=Microbiology%20is%20the%20study%20of,host%20response%20to%20these%20agents.",
+  },
+  "OM": {
+    title: "Openbaar Ministerie",
+    href: "https://www.om.nl/organisatie",
+  },
+  "PBW": {
+    title: "Penitentiaire beginselenwet",
+    href: "https://wetten.overheid.nl/BWBR0009709/2022-10-01",
+  },
+  "Parlementnl": {
+    title: "Denederlandsegrondwet.nl",
+    href: "https://www.denederlandsegrondwet.nl/",
+  },
+  "Politie": {
+    title: "Website van de Nationale Politie",
+    href: "https://www.politie.nl/",
+  },
+  "Politiewet": {
+    title: "Politiewet",
+    href: "https://wetten.overheid.nl/BWBR0031788/2024-08-01",
+  },
+  "Politiewet2012": {
+    title: "Politiewet 2012",
+  },
+  "Postwet": {
+    title: "Postwet 2009",
+    href: "https://www.overheid.nl/",
+  },
+  "Postwet2009": {
+    title: "Postwet 2009",
+  },
+  "Pw": {
+    title: "Politiewet 2012",
+  },
+  "RIVM-1": {
+    title: "Rijksinstituut voor Volksgezondheid en Milieu",
+    href: "https://rvs.rivm.nl/onderwerpen/normen/werkende/grenswaarden#:~:text=De%20grenswaarde%20is%20de%20maximaal,en%2Fof%2015%20minuten).",
+  },
+  "RIVM-2": {
+    title: "Infectieziekten en Veiligheid",
+    href: "https://www.rivm.nl/bibliotheek/digitaaldepot/wrr_infectieziekten.pdf",
+  },
+  "RO": {
+    title: "Wet op de rechterlijke organisatie",
+    href: "https://wetten.overheid.nl/BWBR0001830/2023-07-01#Hoofdstuk2_Afdeling2_Paragraaf1_Artikel21b",
+  },
+  "RR1995": {
+    title: "Reclasseringsregeling",
+  },
+  "RVV1990": {
+    title: "Reglement verkeersregels en verkeerstekens 1990",
+  },
+  "RaadvoorRechtsbijstand": {
+    title: "Website van Raad voor Rechtsbijstand",
+    href: "https://www.raadvoorrechtsbijstand.org/",
   },
   "Rechtspraak": {
-    title: "Juridische begrippen - Rechtspraak",
-    href: "https://www.rechtspraak.nl/juridische-begrippen/Paginas/default.aspx",
+    title: "De Rechtspraak: Juridische begrippen",
+    href: "https://www.rechtspraak.nl/",
+  },
+  "Rechtspraaknl": {
+    title: "Juridische begrippen Rechtspraak.nl",
+    href: "https://www.rechtspraak.nl/juridische-begrippen",
+  },
+  "Rijksoverheid": {
+    title: "Rijksoverheid",
+  },
+  "Rv": {
+    title: "Wetboek van Burgerlijke Rechtvordering",
+    href: "https://wetten.overheid.nl/BWBR0001827/2025-03-08",
+  },
+  "Sr": {
+    title: "Wetboek van Strafrecht",
+    href: "https://wetten.overheid.nl/BWBR0001854/2025-01-01",
+  },
+  "StaatscourantvanhetKoninkrijkderNederlanden": {
+    title: "Website van de Staatscourant van het Koninkrijk der Nederlanden",
+    href: "https://zoek.officielebekendmakingen.nl/stcrt-2020-39755",
   },
   "Sv": {
-    title: "Nieuw Wetboek van Strafvordering",
+    title: "Nieuwe wetboek van Strafvordering",
+    href: "https://www.overheid.nl/",
+  },
+  "ThesaurusPolitieacademie": {
+    title: "Thesaurus Politieacademie",
+  },
+  "VKJP": {
+    title: "Vereniging voor Kinder- en Jeugdpsychotherapie",
+    href: "https://www.vkjp.nl/de-praktijk/wet-en-regelgeving/jeugdstrafrecht#:~:text=Jeugddetentie%20is%20een%20maatregel%20die,en%20terugkeer%20in%20de%20maatschappij.",
+  },
+  "Vreemdelingenwet2000": {
+    title: "Vreemdelingenwet 2000",
+  },
+  "WODC": {
+    title: "Genderneutrale formulering van wetgeving, WODC",
+    href: "https://repository.wodc.nl/bitstream/handle/20.500.12832/3431/3416-genderneutrale-formulering-van-wetgeving-rapport.pdf?isAllowed=y&sequence=1&utm_source=chatgpt.com",
+  },
+  "WRO": {
+    title: "Wet op Rechterlijk Organisatie",
+    href: "https://wetten.overheid.nl/BWBR0001830/2023-07-01",
+  },
+  "WSv": {
+    title: "Wetboek van Strafvordering",
+    href: "https://www.rijksoverheid.nl/binaries/rijksoverheid/documenten/publicaties/2020/12/11/ambtelijke-versie-juli-2020-wetsvoorstel-wetboek-van-strafvordering-boek-5/Wetsvoorstel+nieuw+Wetboek+van+Strafvordering+Boek+5.pdf",
+  },
+  "Wbo": {
+    title: "Wet op de bijzondere opsporingsdiensten",
+  },
+  "WetBRP": {
+    title: "Wet basisregistratie personen",
+  },
+  "WetBedigdetolkenenvertalers": {
+    title: "Bureau Wbtv",
+    href: "https://wetten.overheid.nl/BWBR0022704/2018-07-28",
+  },
+  "WetBig": {
+    title: "Wet op de beroepen in de individuele gezondheidszorg",
+  },
+  "Wetopdebijzondereopsporingsdiensten": {
+    title: "Wet op de bijzondere opsporingsdiensten",
+    href: "https://wetten.overheid.nl/BWBR0019919/2021-05-07",
+  },
+  "Wfz": {
+    title: "Wet forensische zorg",
+    href: "https://wetten.overheid.nl/BWBR0040634/2025-02-28",
+  },
+  "Wots": {
+    title: "Wet overdracht tenuitvoerlegging strafvonnissen",
+  },
+  "Wrb": {
+    title: "Wet op de Rechtsbijstand",
+  },
+  "Wro": {
+    title: "Wet op de rechterlijke organisatie",
+  },
+  "ZiekenhuisGelderseVallei": {
+    title: "'MRI-onderzoek van het hart (met stress)'', Ziekenhuis Gelderse Vallei",
+    href: "https://www.geldersevallei.nl/patient/onderzoeken/mri-onderzoek-met-stress",
+  },
+  "juridischwoordenboeknl": {
+    title: "juridischwoordenboek.nl",
+  },
+  "omnl": {
+    title: "om.nl",
+  },
+  "rechtspraaknl": {
+    title: "rechtspraak.nl",
   },
 }
